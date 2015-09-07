321094ceeb91a37418db0d1203a5bec1f2f4ec22
Edward Coleridge Smith <edward.smith@pod-point.com>
2015-09-07 10:26:34 +0100

    Added country lists

---
+++ src/config/countries-partial.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| List of Countries
+|--------------------------------------------------------------------------
+|
+| A partial list of country names indexed by their uppercase ISO country
+| code; to be used when we don't need to display a full list of countries
+| to the end user.
+|
+*/
+
+return [
+    'NO' => 'Norway',
+    'GB' => 'United Kingdom'
+];
