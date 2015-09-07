1abfb1b8fef25154c4260ab9d307312556be5901
Edward Coleridge Smith <edward.smith@pod-point.com>
2015-09-07 10:44:23 +0100

    Added the countries service provider

---
+++ src/Providers/CountriesProvider.php
@@ -0,0 +1,31 @@
+<?php namespace PodPoint\Admin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CountriesProvider extends ServiceProvider
+{
+    /**
+     * The configuration files to be loaded indexed by the configuration names.
+     *
+     * @var array
+     */
+    private $config = [
+        'countries'         => __DIR__ . '../config/countries.php',
+        'countries-partial' => __DIR__ . '../config/countries-partial.php'
+    ];
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        /** @var \Illuminate\Config\Repository $config */
+        $config = $this->app['config'];
+
+        foreach ($this->config as $index => $filename) {
+            $config->set('countries', require $filename);
+        }
+    }
+}
