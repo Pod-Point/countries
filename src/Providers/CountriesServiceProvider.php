3e3b19c8efb28fc86276c5e6434733a1d5ac342e
Lee Brooks <leebrooks93@gmail.com>
2018-01-31 16:29:34 +0000

    Add ISO country information to existing country config
    When registering country configuration files we also append country information from an ISO countries package.

--- src/Providers/CountriesServiceProvider.php
+++ src/Providers/CountriesServiceProvider.php
@@ -3,6 +3,7 @@
 namespace PodPoint\Countries\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use League\ISO3166\ISO3166;
 
 class CountriesServiceProvider extends ServiceProvider
 {
@@ -25,9 +26,42 @@ class CountriesServiceProvider extends ServiceProvider
     {
         /** @var \Illuminate\Config\Repository $config */
         $config = $this->app->config;
+        $countryConfigFiles = ['countries', 'countries-partial'];
 
         foreach ($this->config as $filename) {
-            $config->set($filename, require __DIR__ . '/../config/' . $filename . '.php');
+            $data = require __DIR__ . '/../config/' . $filename . '.php';
+
+            if (in_array($filename, $countryConfigFiles)) {
+                $data = $this->addIsoInfoToCountryConfig($data, $filename);
+            }
+
+            $config->set($filename, $data);
         }
+    }
+
+    /**
+     * Adds ISO country information to our existing country configuration.
+     *
+     * @param array $data
+     * @param string $filename
+     *
+     * @return array
+     */
+    private function addIsoInfoToCountryConfig(array $data, string $filename)
+    {
+        $isoCountries = (new ISO3166())->all();
+
+        foreach ($isoCountries as $isoData) {
+            $countryCode = $isoData['alpha2'];
+            $configData = array_get($data, $countryCode, []);
+
+            if ($filename === 'countries-partial' && !$configData) {
+                continue;
+            }
+
+            array_set($data, $countryCode, array_merge($configData, $isoData));
+        }
+
+        return $data;
     }
 }
