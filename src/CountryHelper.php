86770200f4d060bf7a4d2e289956ad8043221a13
Richard Toms <richard@toms.dev>
2021-05-19 18:46:36 +0100

    [3.x] [SWP-2788] Introduce memoization for efficiency during use on mass (#38)
    **Changes**
    - Introduced a history for finds in the CountryHelper so that we don't perform the same lookup many times
    - Introduced a caching layer for NumberFormatters used in the CurrencyHelper to avoid creating many unnecessary instances when used on mass

--- src/CountryHelper.php
+++ src/CountryHelper.php
@@ -5,6 +5,13 @@ namespace PodPoint\I18n;
 class CountryHelper extends Helper
 {
     /**
+     * Recent lookup results by `property-value`.
+     *
+     * @var array
+     */
+    protected $results = [];
+
+    /**
      * Returns the first country matching the given property/value pair.
      *
      * @param string $property
@@ -14,7 +21,13 @@ class CountryHelper extends Helper
      */
     public function findBy(string $property, $value): ?array
     {
-        return collect($this->config->get('countries'))
+        $key = $this->generateResultsKey($property, $value);
+
+        if ($this->results[$key] ?? false) {
+            return $this->results[$key];
+        }
+
+        return $this->results[$key] = collect($this->config->get('countries'))
             ->where($property, $value)
             ->first();
     }
@@ -44,5 +57,17 @@ class CountryHelper extends Helper
             });
 
         return $code ? $code : null;
+    }
+
+    /**
+     * Generate a string formed of the property and value for the results cache.
+     *
+     * @param string $property
+     * @param string $value
+     * @return string
+     */
+    protected function generateResultsKey(string $property, string $value): string
+    {
+        return "${property}-{$value}";
     }
 }
