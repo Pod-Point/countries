4f25ee383871fd2b8d3aedd784fc875dfb577807
Francesco <francesco.pepe@pod-point.com>
2022-04-29 15:58:34 +0100

    [3.x] Adds precision to the NumberHelper and implements NumberHelper Facade (#57)

--- src/NumberHelper.php
+++ src/NumberHelper.php
@@ -7,19 +7,29 @@ use NumberFormatter;
 class NumberHelper extends LocalizedHelper
 {
     /**
-     * Return a number formatted for the given locale.
+     * Return a number formatted for the given locale. Specify precision to define the exact number of decimal digits.
      *
      * @param  float|int  $value
      * @param  string  $locale
+     * @param  int|null  $precision
      * @return string
      */
-    public function toFormat($value, string $locale = 'en'): string
+    public function toFormat($value, string $locale = 'en', int $precision = null): string
     {
-        $formatter = new NumberFormatter(
-            $this->getSystemLocale($locale),
-            NumberFormatter::DEFAULT_STYLE
-        );
+        if (is_null($precision)) {
+            return $this->getDefaultFormatter($locale)->format($value);
+        }
 
-        return $formatter->format($value);
+        return $this->getFixedPrecisionFormatter($locale, $precision)->format($value);
+    }
+
+    /**
+     * Return the formatter style used by the number formatter.
+     *
+     * @return int
+     */
+    protected function getFormatterStyle(): int
+    {
+        return NumberFormatter::DEFAULT_STYLE;
     }
 }
