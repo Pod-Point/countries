2ef36a2cd3c4cb6387e5ad4611ba24a6328cd3f7
Clem Blanco <clement.blanco@pod-point.com>
2023-12-06 17:12:53 +0100

    fix(v4): locale for Ireland
    They speak English in Ireland. I think.
    
    We should use `{locale/language}_{country_code_alpha2}`
    
    - English (United Kingdom): `en_GB`
    - English (Ireland): `en_IE`
    - Norwegian (Norway): `no_NO`
    
    `ie_IE` doesn't exist.
    
    Source: https://saimana.com/list-of-country-locale-code/

--- config/countries-partial.php
+++ config/countries-partial.php
@@ -28,7 +28,7 @@ return [
     ],
     CountryCode::IRELAND => [
         'systemLocale' => 'en_IE.UTF-8',
-        'locale' => 'ie',
+        'locale' => 'en',
         'language' => 'ENG',
         'tld' => 'ie',
         'timezone' => 'Europe/Dublin',
